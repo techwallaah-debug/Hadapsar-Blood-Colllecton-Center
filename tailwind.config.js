d840d83bb70fd75a4ac6ede519baf2bda374fc54
sangam <techwallaah@gmail.com>
2026-06-24 12:59:25 +0530

    fix: replace Tailwind CDN with local build, remove duplicate script.js, fix badgeClass error

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html',
+    './includes/**/*.html',
+    './admin-pannubai/**/*.html',
+    './js/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
